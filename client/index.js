ba4d3c6f4c90fae0a3a160a4bdc80cc7d7ff4f9e
exdeejay <13634296+exdeejay@users.noreply.github.com>
2022-09-22 08:34:28 -0400

    remove unneeded constant whoops

--- client/index.js
+++ client/index.js
@@ -1,4 +1,3 @@
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const net = require('net');
 const { loadPacketTypes } = require('../lib/protocol');
 const registry = require('./registry');
